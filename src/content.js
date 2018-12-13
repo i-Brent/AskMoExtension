60d09d8dc8eca65274181fe4d8fd805500b03cf8
Brent Schroder <i-brent@edmodo.com>
2018-12-13 14:58:22 -0800

    pre-eject

---
+++ src/content.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "./content.css";
+
+class Main extends Component {
+    render() {
+        return (
+            <div className={'my-extension'}>
+                <h1>Hello world - My first Extension</h1>
+            </div>
+        )
+    }
+}
+
+const app = document.createElement('div');
+app.id = "my-extension-root";
+document.body.appendChild(app);
+ReactDOM.render(<Main />, app);
+
+app.style.display = "none";
+chrome.runtime.onMessage.addListener(
+   function(request, sender, sendResponse) {
+      if( request.message === "clicked_browser_action") {
+        toggle();
+      }
+   }
+);
+function toggle(){
+   if(app.style.display === "none"){
+     app.style.display = "block";
+   }else{
+     app.style.display = "none";
+   }
+}
